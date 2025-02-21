5eb86ab681fe6501ed9b1a3e53170e25d290cc75
Nguyen Duc Dat (K18 HL) <duylcse04194@fpt.edu.vn>
2025-02-22 00:04:33 +0700

    list all

--- back/src/main/java/fa/pjb/back/service/impl/UserServiceImpl.java
+++ back/src/main/java/fa/pjb/back/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package fa.pjb.back.service.impl;
 
+import fa.pjb.back.model.entity.Parent;
 import fa.pjb.back.model.entity.User;
 import fa.pjb.back.model.vo.UserVO;
 import fa.pjb.back.repository.ParentRepository;
@@ -26,17 +27,39 @@ public class UserServiceImpl implements UserService {
         Page<User> userEntitiesPage = userRepository.findAll(of);
         return userEntitiesPage.map(this::convertToUserVO);
     }
+
     private UserVO convertToUserVO(User user) {
-        if(user.getRole().equals("0")){
+
+        if (user.getRole().equals("0")) {
+            Parent temp = user.getParent();
             return UserVO.builder()
                     .id(user.getId())
-                    .fullname(user.getParent().getFullname())
+                    .fullname(temp.getFullname())
+                    .email(user.getEmail())
+                    .phone(temp.getPhone()) // Assuming phone is null for Parent
+                    .address(temp.getStreet()+temp.getDistrict()+temp.getProvince()+temp.getWard()) // Assuming address is null for Parent
+                    .role("Parent")
+                    .status(user.getStatus() ? "Active" : "Inactive")
                     .build();
-        }else if(user.getRole().equals("1")){
-
-        }else{
-
+        } else if (user.getRole().equals("1")) {
+            return UserVO.builder()
+                    .id(user.getId())
+                    .fullname(user.getSchoolOwner().getFullname())
+                    .email(user.getEmail())
+                    .phone(user.getSchoolOwner().getPhone())
+                    .role("School Owner")
+                    .status(user.getStatus() ? "Active" : "Inactive")
+                    .build();
+        } else {
+            return UserVO.builder()
+                    .id(user.getId())
+                    .fullname(user.getUsername())
+                    .email(user.getEmail())
+                    .phone("") // Assuming phone is null for generic users
+                    .address("") // Assuming address is null for generic users
+                    .role("Admin")
+                    .status(user.getStatus() ? "Active" : "Inactive")
+                    .build();
         }
-        return null;
     }
 }
