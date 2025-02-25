42d0f9ee2f30a2e902cc06c1dd5f9394992db8c5
hieungo <hieunm147@fpt.com>
2025-02-25 14:16:34 +0700

    update userSlice

--- front/src/redux/features/userSlice.ts
+++ front/src/redux/features/userSlice.ts
@@ -2,22 +2,29 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 export interface UsersState {
     username: string,
+    id: string,
 }
 
 const initialState: UsersState = {
     username: '',
+    id: ''
 }
 
 export const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
-        updateUsername: (state, action: PayloadAction<string>) => {
-            state.username = action.payload;
-        }
+        updateUser: (state, action: PayloadAction<{ username: string, id: string }>) => {
+            state.username = action.payload.username;
+            state.id = action.payload.id;
+        },
+        resetUser: (state) => {
+            state.username = initialState.username;
+            state.id = initialState.id;
+        },
     }
 })
 
-export const {updateUsername} = userSlice.actions;
+export const {updateUser, resetUser} = userSlice.actions;
 
 export default userSlice.reducer;
