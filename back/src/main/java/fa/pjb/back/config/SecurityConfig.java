8730a4978fd2551f30e0d535b9a70dd35450ea7f
hieungo <hieunm147@fpt.com>
2025-03-07 16:54:40 +0700

    fix School and SchoolOwner entities

--- back/src/main/java/fa/pjb/back/config/SecurityConfig.java
+++ back/src/main/java/fa/pjb/back/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +26,7 @@ import java.util.Collections;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
     public static final String SWAGGER_UI_URL = "/swagger-ui/**";
