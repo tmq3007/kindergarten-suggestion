488fd442e7a733f3bb7a1ddf6657eec115744f64
tmq3007 <quantm3007@gmail.com>
2025-03-13 16:17:07 +0700

    Update for view account, add user

--- front/src/app/components/user/ProfileSideBar.tsx
+++ front/src/app/components/user/ProfileSideBar.tsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Breadcrumb, Avatar, Typography, Upload, message } from 'antd';
+import React, {useState, useEffect} from 'react';
+import {Breadcrumb, Avatar, Typography, Upload, message} from 'antd';
 import Link from 'next/link';
 import {CalendarOutlined, MailOutlined, PhoneOutlined, UserOutlined} from '@ant-design/icons';
 import {motion} from "framer-motion";
+import MyBreadcrumb from "@/app/components/common/MyBreadcrumb";
+import {lato} from "@/lib/fonts";
 
-const { Title } = Typography;
+const {Title} = Typography;
 
 interface ProfileSidebarProps {
     fullname: string | undefined;
@@ -15,7 +17,7 @@ interface ProfileSidebarProps {
     onAvatarChange?: (file: File) => void;
 }
 
-const ProfileSidebar: React.FC<ProfileSidebarProps> = ({ fullname, email, phone, dob, avatarUrl, onAvatarChange }) => {
+const ProfileSidebar: React.FC<ProfileSidebarProps> = ({fullname, email, phone, dob, avatarUrl, onAvatarChange}) => {
     const [avatar, setAvatar] = useState<string | undefined>(avatarUrl || '/bg3.jpg');
     const [fileList, setFileList] = useState<any[]>([]);
 
@@ -41,19 +43,16 @@ const ProfileSidebar: React.FC<ProfileSidebarProps> = ({ fullname, email, phone,
     return (
         <div className="h-full p-6 rounded-xl flex flex-col items-center space-y-6">
             <div className="w-full">
-                <Breadcrumb
-                    className="text-black"
-                    items={[
-                        { title: <Link href="/public" className="text-blue-600 hover:underline">Home</Link> },
-                        { title: 'My Profile' },
-                    ]}
-                />
+                <MyBreadcrumb paths={[
+                    {label: 'Home', href: '/public'},
+                    {label: 'My Profile'}
+                ]}/>
             </div>
 
-            <motion.h4  initial={{ color: "#019376" }}
-                        animate={{ color: ["#3f51b5", "#019376"] }}
-                        transition={{ duration: 3, repeat: Infinity }}
-                        className="text-lg font-semibold"
+            <motion.h4 initial={{color: "#019376"}}
+                       animate={{color: ["#3f51b5", "#019376"]}}
+                       transition={{duration: 3, repeat: Infinity}}
+                       className="text-lg mt-0 font-semibold"
             >
                 My Profile
             </motion.h4>
@@ -69,36 +68,37 @@ const ProfileSidebar: React.FC<ProfileSidebarProps> = ({ fullname, email, phone,
                     <Avatar
                         size={150}
                         src={avatar}
-                        icon={<UserOutlined />}
+                        icon={<UserOutlined/>}
                         className="cursor-pointer border-4 border-white shadow-lg mx-auto"
                     />
                 </Upload>
             </div>
 
             <div className="text-center space-y-2 w-full max-w-xs">
-                <Title level={4} className="text-black">
+                <Title level={2} className={`${lato.className} text-black`}>
                     {fullname || 'N/A'}
                 </Title>
 
             </div>
 
             <div className="w-full text-left max-w-xs flex flex-col items-start space-y-4">
-            <div>
-                    <Title level={5} className="text-black">
-                        <MailOutlined className="mr-2" /> {/* Icon cho Email */}
-                        {email || 'N/A'}
-                    </Title>
+                <div className="flex items-center justify-center space-x-2">
+                    <label className="text-black font-medium">
+                        <MailOutlined className="mr-2"/>
+                        Email:
+                    </label>
+                    <p className="text-black font-medium"> {email || 'N/A'}</p>
                 </div>
                 <div className="flex items-center justify-center space-x-2">
                     <label className="text-black font-medium">
-                        <CalendarOutlined className="mr-2" /> {/* Icon cho Date of Birth */}
+                        <CalendarOutlined className="mr-2"/>
                         Date of Birth:
                     </label>
                     <p className="text-black font-medium">{dob || 'N/A'}</p>
                 </div>
                 <div className="flex items-center justify-center space-x-2">
                     <label className="text-black font-medium">
-                        <PhoneOutlined className="mr-2" /> {/* Icon cho Phone Number */}
+                        <PhoneOutlined className="mr-2"/>
                         Phone Number:
                     </label>
                     <p className="text-black font-medium">{phone || 'N/A'}</p>
