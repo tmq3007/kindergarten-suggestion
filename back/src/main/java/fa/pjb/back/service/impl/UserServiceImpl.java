e96060cd93f8007ec969dbd99c872cdb0510106a
tmq3007 <quantm3007@gmail.com>
2025-03-17 15:54:56 +0700

    Update for view account, add user

--- back/src/main/java/fa/pjb/back/service/impl/UserServiceImpl.java
+++ back/src/main/java/fa/pjb/back/service/impl/UserServiceImpl.java
@@ -2,42 +2,55 @@ package fa.pjb.back.service.impl;
 
 import fa.pjb.back.common.exception._11xx_email.EmailAlreadyExistedException;
 import fa.pjb.back.common.exception._14xx_data.InvalidDateException;
+import fa.pjb.back.common.exception._14xx_data.InvalidFileFormatException;
+import fa.pjb.back.common.exception._14xx_data.UploadFileException;
 import fa.pjb.back.common.util.AutoGeneratorHelper;
 import fa.pjb.back.model.dto.UserCreateDTO;
 import fa.pjb.back.model.dto.UserDetailDTO;
 import fa.pjb.back.model.dto.UserUpdateDTO;
-import fa.pjb.back.model.entity.Parent;
-import fa.pjb.back.model.entity.SchoolOwner;
-import fa.pjb.back.model.entity.User;
+import fa.pjb.back.model.entity.*;
 import fa.pjb.back.model.enums.ERole;
 import fa.pjb.back.model.mapper.UserMapper;
 import fa.pjb.back.model.mapper.UserProjection;
+import fa.pjb.back.model.vo.ImageVO;
 import fa.pjb.back.model.vo.UserVO;
+import fa.pjb.back.repository.MediaRepository;
 import fa.pjb.back.repository.ParentRepository;
 import fa.pjb.back.repository.SchoolOwnerRepository;
 import fa.pjb.back.repository.UserRepository;
 import fa.pjb.back.service.AuthService;
 import fa.pjb.back.service.EmailService;
+import fa.pjb.back.service.GGDriveImageService;
 import fa.pjb.back.service.UserService;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tika.Tika;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import static fa.pjb.back.model.enums.ERole.*;
+import static fa.pjb.back.model.enums.FileFolderEnum.SCHOOL_IMAGES;
+import static fa.pjb.back.model.enums.FileFolderEnum.SO_IMAGES;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
+    private static final long MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+    private static final List<String> ALLOWED_MIME_TYPES = Arrays.asList("image/jpeg", "image/png", "image/jpg");
+    private final GGDriveImageService imageService;
+    private static final Tika tika = new Tika();
+    private final MediaRepository mediaRepository;
 
     private final UserMapper userMapper;
     private final UserRepository userRepository;
@@ -157,8 +170,51 @@ public class UserServiceImpl implements UserService {
         Boolean.TRUE.equals(user.getStatus()) ? "Active" : "Inactive");
   }
 
+    private void processAndSaveImages(List<MultipartFile> image, SchoolOwner schoolOwner) {
+        List<ImageVO> imageVOList;
+
+        for (MultipartFile file : image) {
+            if (file.getSize() > MAX_FILE_SIZE) {
+                throw new InvalidFileFormatException("File cannot exceed 5MB");
+            }
+            try {
+                String mimeType = tika.detect(file.getBytes());
+                if (!ALLOWED_MIME_TYPES.contains(mimeType)) {
+                    throw new InvalidFileFormatException("Invalid file type: " + file.getOriginalFilename() + ". Allowed: JPEG, PNG, JPG.");
+                }
+            } catch (IOException e) {
+                throw new InvalidFileFormatException("Error when processing file");
+            }
+        }
+
+        try {
+            imageVOList = imageService.uploadListImages(
+                    imageService.convertMultiPartFileToFile(image),
+                    "School_Owner_" + schoolOwner.getId() + "Image_",
+                    SO_IMAGES
+            );
+        } catch (IOException e) {
+            throw new UploadFileException("Error while uploading images: " + e.getMessage());
+        }
+        List<Media> mediaList = new ArrayList<>();
+        for (ImageVO imageVO : imageVOList) {
+            if (imageVO.status() == 200) {
+                Media media = Media.builder()
+                        .url(imageVO.url())
+                        .size(String.valueOf(imageVO.size()))
+                        .cloudId(imageVO.fileId())
+                        .filename(imageVO.fileName())
+                        .type("image/png")
+                        .uploadTime(LocalDate.now())
+                        .schoolOwner(schoolOwner)
+                        .build();
+                mediaList.add(media);
+            }
+        }
+        mediaRepository.saveAll(mediaList);
+    }
     @Override
-    public UserCreateDTO createUser(UserCreateDTO userCreateDTO) {
+    public UserCreateDTO createUser(UserCreateDTO userCreateDTO, List<MultipartFile> image) {
         if (userCreateDTO.email() == null || userCreateDTO.email().isBlank()) {
             throw new IllegalArgumentException("Email cannot be empty");
         }
@@ -173,6 +229,7 @@ public class UserServiceImpl implements UserService {
         if (userCreateDTO.dob() == null || !userCreateDTO.dob().isBefore(LocalDate.now())) {
             throw new InvalidDateException("Dob must be in the past");
         }
+
         // Create User
         String usernameAutoGen = autoGeneratorHelper.generateUsername(userCreateDTO.fullname());
         String passwordAutoGen = autoGeneratorHelper.generateRandomPassword();
@@ -181,7 +238,7 @@ public class UserServiceImpl implements UserService {
                 .password(passwordEncoder.encode(passwordAutoGen))
                 .phone(userCreateDTO.phone())
                 .fullname(userCreateDTO.fullname())
-                .status(Boolean.valueOf(userCreateDTO.status()))
+                .status(userCreateDTO.status())
                 .dob(userCreateDTO.dob())
                 .email(userCreateDTO.email())
                 .build();
@@ -195,10 +252,16 @@ public class UserServiceImpl implements UserService {
                     .publicPermission(true)
                     .school(null)
                     .assignTime(LocalDate.from(LocalDateTime.now()))
+                    .business_registration_number(userCreateDTO.business_registration_number())
                     .build();
 
             // Save SchoolOwner to database
-            schoolOwnerRepository.save(schoolOwner);
+            SchoolOwner newSO = schoolOwnerRepository.save(schoolOwner);
+
+           // Validate and upload images (if provided)
+           if (image != null && !image.isEmpty()) {
+               processAndSaveImages(image, newSO);
+           }
         } else if(Objects.equals(userCreateDTO.role(), "ROLE_ADMIN")) {
             user.setRole(ERole.ROLE_ADMIN);
         }
@@ -206,7 +269,8 @@ public class UserServiceImpl implements UserService {
         // Save User to database
         userRepository.save(user);
 
-        UserCreateDTO responseDTO = userMapper.toUserDTO(user);
+
+        //UserCreateDTO responseDTO = userMapper.toUserDTO(user);
 
         //Check email exist
         if (existingUserEmail.isEmpty()) {
@@ -214,7 +278,7 @@ public class UserServiceImpl implements UserService {
                     usernameAutoGen,passwordAutoGen);
         }
 
-        return responseDTO;
+        return userCreateDTO;
     }
 
 }
